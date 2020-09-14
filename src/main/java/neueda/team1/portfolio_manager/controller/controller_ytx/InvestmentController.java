baf9f76821138cc6dda050cf5b592106aa5a2335
Yu_Tongxin <1303052820@qq.com>
2020-09-14 02:06:17 +0000

    2020-09-14 commited by Yu Tongxin, add default GetMapping for /investmentinfo to get all detailed including current data of portfolio

--- src/main/java/neueda/team1/portfolio_manager/controller/controller_ytx/InvestmentController.java
+++ src/main/java/neueda/team1/portfolio_manager/controller/controller_ytx/InvestmentController.java
@@ -1,9 +1,16 @@
 package neueda.team1.portfolio_manager.controller.controller_ytx;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import neueda.team1.portfolio_manager.entity.domain_ytx.Portfolio;
+import neueda.team1.portfolio_manager.service.service_ytx.PortfolioService;
 import neueda.team1.portfolio_manager.service.service_ytx.httputils_ytx.TrochilHttpManage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * InvestmentController class
@@ -22,9 +29,27 @@ public class InvestmentController {
     @Autowired
     private TrochilHttpManage trochilHttpManage;
 
+    @Autowired
+    PortfolioService portfolioService;
+
     @GetMapping(value="/{symbol}", produces={"application/json"})
-    public ResponseEntity<String> getAllPortfolios(@PathVariable String symbol) {
+    public ResponseEntity<String> getInvestmentBySymbol(@PathVariable String symbol) {
         String result = trochilHttpManage.getInvestmentBySymbol(symbol);
         return ResponseEntity.ok().body(result);
     }
+
+    @GetMapping(value="", produces={"application/json"})
+    public ResponseEntity<String> getAllInvestment() {
+
+        List<Portfolio>portfolios = portfolioService.findAll();
+        JsonArray jsonResultArray = new JsonArray();
+        for(Portfolio portfolio:portfolios){
+            String investment = trochilHttpManage.getInvestmentBySymbol(portfolio.getSymbol());
+            JsonElement jsonElement = JsonParser.parseString(investment);
+            jsonResultArray.add(jsonElement);
+        }
+        return ResponseEntity.ok().body(jsonResultArray.toString());
+    }
+
+
 }
