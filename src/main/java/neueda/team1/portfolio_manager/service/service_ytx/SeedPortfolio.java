2e7159a6b54be61f267a60e700e1b00cb69eb500
Yu_Tongxin <1303052820@qq.com>
2020-09-14 02:06:38 +0000

    2020-09-14 commited by Yu Tongxin, add default GetMapping for /investmentinfo to get all detailed including current data of portfolio

--- src/main/java/neueda/team1/portfolio_manager/service/service_ytx/SeedPortfolio.java
+++ src/main/java/neueda/team1/portfolio_manager/service/service_ytx/SeedPortfolio.java
@@ -21,15 +21,11 @@ public class SeedPortfolio {
     public void init(){
 
         repository.save(new Portfolio("Apple Inc.", "AAPL", 112,  100));
-        repository.save(new Portfolio("Nikola Corporation", "NKLA", 32.13,  100));
-        repository.save(new Portfolio("Peloton Interactive", "PTON", 84.04,  100));
         repository.save(new Portfolio("General Electric Company", "GE", 5.95,  100));
         repository.save(new Portfolio("Tesla, Inc.", "TSLA", 372.72,  100));
         repository.save(new Portfolio("Ford Motor Company", "F", 7,  100));
         repository.save(new Portfolio("Advanced Micro Devices, Inc.", "AMD", 76.34,  100));
-        repository.save(new Portfolio("NIO Limited", "NIO", 17.97,  100));
         repository.save(new Portfolio("Bank of America", "BAC", 25.5,  100));
-        repository.save(new Portfolio("Vale S.A.",  "VALE", 11.67, 100));
     }
 
 }
