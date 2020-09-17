b9e3880b6501e817b41dc8d9e36c7289fbd050cb
Cao, Yu <caoyu.work@hotmail.com>
2020-09-17 09:38:12 +0800

    Make CashFLow and NetWorth controller cross-origin

--- src/main/java/neueda/team1/portfolio_manager/controller/CashFlowController.java
+++ src/main/java/neueda/team1/portfolio_manager/controller/CashFlowController.java
@@ -2,13 +2,11 @@ package neueda.team1.portfolio_manager.controller;
 
 import neueda.team1.portfolio_manager.service.CashFlowService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping("cashFlow")
 public class CashFlowController {
--- src/main/java/neueda/team1/portfolio_manager/controller/NetWorthController.java
+++ src/main/java/neueda/team1/portfolio_manager/controller/NetWorthController.java
@@ -2,13 +2,11 @@ package neueda.team1.portfolio_manager.controller;
 
 import neueda.team1.portfolio_manager.service.NetWorthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("netWorth")
 public class NetWorthController {
